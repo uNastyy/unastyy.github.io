e1246f5a5bbe4c49fed9e9f37a065369a75480af
uNastyy_ <119569179+uNastyy@users.noreply.github.com>
2025-02-11 22:05:17 +0100

    delete useless fonction
    j'ai passé 30min à corriger les erreur d'import etc de cette fonction alors qu'elle sert à RIEN

--- script.js
+++ script.js
@@ -16,23 +16,6 @@ $(document).ready(function () {
   // input text for typing animation
   $("#holder").writeText("Etudiant en informatique");
 
-  (function () {
-    "use strict";
-
-    const navbar = document.querySelector(".banner-text");
-
-    document.addEventListener("scroll", (e) => {
-      const scrolled = document.scrollingElement.scrollTop;
-
-      if (scrolled > 80) {
-        //adjust to suit your need of when transition start
-        navbar.classList.add("scrolled");
-      } else {
-        navbar.classList.remove("scrolled");
-      }
-    });
-  })();
-
   // Code pour changer la classe active des liens de la navbar en fonction de la section visible
   document.addEventListener("DOMContentLoaded", function () {
     const sections = document.querySelectorAll("section");
@@ -51,28 +34,6 @@ $(document).ready(function () {
     window.addEventListener("scroll", changeActiveLink);
   });
 
-  const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    loop: true, // Pour un défilement infini
-});
-new fullpage('#fullpage', {
-  navigation: true,
-  responsiveWidth: 700,
-  anchors: ['accueil', 'a-propos', 'competences', 'projets', 'contact'],
-  parallax: true,
-  onLeave: function(origin, destination, direction){
-      console.log("Leaving section" + origin.index);
-  },
-});
   // initialize wow.js
   new WOW().init();
 });
